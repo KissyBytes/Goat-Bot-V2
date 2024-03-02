1f9d4329ee7c69be56e59b48dd7247f9873b7141
KissyBytes <156104758+KissyBytes@users.noreply.github.com>
2024-03-02 16:55:26 +0800

    Create react.js

---
+++ react.js
@@ -0,0 +1,25 @@
+module.exports = {
+    config: {
+        name: "autoreact",
+		      version: "1.0",
+	       	author: "Loid Butter",
+		      countDown: 5,
+	       	role: 0,
+		      shortDescription: "",
+	       	longDescription: "",
+		       category: "dont know ",
+    },
+	onStart: async function (){},
+	onChat: async function ({ event ,api}) {
+		if (event.body.toLowerCase().indexOf("iloveyou") !== -1) return api.setMessageReaction("😼", event.messageID,event.threadID)
+		
+		if (event.body.toLowerCase().indexOf("good night") !== -1) return api.setMessageReaction("💕", event.messageID,event.threadID)
+		
+		if (event.body.toLowerCase().indexOf("good morning") !== -1) return api.setMessageReaction("😸", event.messageID,event.threadID)
+		
+		if (event.body.toLowerCase().indexOf("pakyo") !== -1) return api.setMessageReaction("😾", event.messageID,event.threadID)
+		
+		if (event.body.toLowerCase().indexOf("mahal") !== -1) return api.setMessageReaction("💩", event.messageID,event.threadID)
+		
+		if (event.body.toLowerCase().indexOf("mwa") !== -1) return api.setMessageReaction("💗", event.messageID,event.threadID)
+        
